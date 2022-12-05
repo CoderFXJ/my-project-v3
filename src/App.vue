35714a21401b55fa8efaed0ef39e122312f686b9
FXJ <888888@qq.com>
2022-12-06 00:06:40 +0800

    ✨ 成功读取到图片信息

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import BoxDemo from './views/01_3d_box/index.vue'
 </script>
 
 <template>
+  <BoxDemo/>
 </template>
 
 <style scoped>
---
+++ src/views/01_3d_box/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="img-wrap" :style="imgStyle">
+   
+  </div>
+</template>
+
+<script lang='ts' setup>
+import { reactive ,computed} from 'vue'
+import img1 from '../../assets/images/01.png'
+const img = new Image()
+img.src = img1
+//输出图片宽高
+const imgInfo = reactive({
+  width: 0,
+  height: 0
+})
+img.onload = function () {
+  imgInfo.width = img.width
+  imgInfo.height = img.height
+  console.log('imgInfo: ', imgInfo);
+}
+//转换样式
+const imgStyle = computed(() => {
+  const { width, height } = imgInfo
+  return {
+    width: `${width}px`,
+    height: `${height}px`,
+    backgroundImage: `url(${img1})`,
+    backgroundPosition: 'center',
+  }
+})
+
+</script>
+<style scoped lang='scss'>
+.img-wrap{
+  border:1px solid #ccc;
+}
+</style>
